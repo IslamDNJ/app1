89aa1c5a58dfb9824b18fdcbbaf639980efc3111
IslamDNJ <syleimanov20148@gmail.com>
2024-10-14 18:07:54 +0300

    Create superUser and 1 Table

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# Поле ID создается автоматически в фале app/settings.py > DEFAULT_AUTO_FIELD
+# SlugField - Хранит фрагменты URL-адресов       // varchar
+# CharField - Для строк небольшого размера       // varchar
+# unique - уникальность
+# blank - пустое значение
+# null - нулевое значение
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True,)
+    slur = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = "category"
