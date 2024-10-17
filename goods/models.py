c69e4c18bed38e0c3fde6b25d5ed6946ecf78231
IslamDNJ <syleimanov20148@gmail.com>
2024-10-17 18:08:12 +0300

    Ease registration models django in Admin

--- goods/models.py
+++ goods/models.py
@@ -8,11 +8,14 @@ from django.db import models
 # unique - уникальность
 # blank - пустое значение
 # null - нулевое значение
-
+# verbose_name - изменяет название в папке
+# verbose_name_plural - изменяет название самой папки
 
 class Categories(models.Model):
-    name = models.CharField(max_length=150, unique=True,)
-    slur = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slur = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
 
     class Meta:
         db_table = "category"
+        verbose_name ='Категорию'
+        verbose_name_plural ='Категории'
