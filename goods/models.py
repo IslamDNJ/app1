cb5bb6f4c87d413f0259bac4b30876e17b555b67
IslamDNJ <syleimanov20148@gmail.com>
2024-10-22 21:56:36 +0300

    Correct display of the category name in the admin panel

--- goods/models.py
+++ goods/models.py
@@ -21,6 +21,9 @@ class Categories(models.Model):
         verbose_name ='Категорию'
         verbose_name_plural ='Категории'
 
+    def __str__(self):
+        return self.name
+
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
     slur = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
@@ -35,3 +38,6 @@ class Products(models.Model):
         db_table = "product"
         verbose_name ='Продукт'
         verbose_name_plural ='Продукты'
+
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
