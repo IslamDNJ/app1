6bf9dec0318342b518526ebae8a4a39026ef2616
IslamDNJ <syleimanov20148@gmail.com>
2024-11-02 14:16:31 +0300

    Changed "blank" to "price" and Added correct output of prices and discounts from the database for goods

--- goods/models.py
+++ goods/models.py
@@ -29,7 +29,7 @@ class Products(models.Model):
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(max_length=10000,blank=True, null=True, verbose_name='Описание')
     image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
-    blank = models.DecimalField(default=0.00,max_digits=7, decimal_places=2, verbose_name='Цена')
+    price = models.DecimalField(default=0.00,max_digits=7, decimal_places=2, verbose_name='Цена')
     discount = models.DecimalField(default=0.00,max_digits=4, decimal_places=2, verbose_name='Скидка в %')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
@@ -44,3 +44,9 @@ class Products(models.Model):
     
     def display_id(self):
         return f"{self.id:05}"
+    
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price* self.discount/100, 2)
+        
+        return self.price
