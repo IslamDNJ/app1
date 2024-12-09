10afe0093c4eaa4cb4b96b7cc53a9db5ccd0d331
IslamDNJ <syleimanov20148@gmail.com>
2024-12-09 17:45:20 +0300

    Carts - ajax - adding and applying ajax in a project

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 
 from carts.models import Cart
+from carts.templatetags.carts_tags import user_carts
 from goods.models import Products
 
-def cart_add(request, product_slug):
-    product = Products.objects.get(slug=product_slug)
+def cart_add(request):
+    
+    product_id = request.POST.get("product_id")
+
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -17,8 +23,16 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])
+    cart_item_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_carts}, request=request
+    )
 
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_item_html,
+    }
+
+    return JsonResponse(response_data)
 
 def cart_change(request, product_slug):
     ...
