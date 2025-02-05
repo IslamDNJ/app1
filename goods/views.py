d8bf41330e79de075750fede6f19c3eb32ed4412
IslamDNJ <syleimanov20148@gmail.com>
2025-02-05 23:37:13 +0300

    CBV - Adding ListView to goods and fixing the paginator

--- goods/views.py
+++ goods/views.py
@@ -1,47 +1,48 @@
-from typing import Any
-from django.core.paginator import Paginator
-from django.db.models import QuerySet
-from django.db.models.base import Model as Model
 from django.http import Http404
-from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from goods.models import Products
 from goods.utils import q_search
 
+
 # Контролер для каталога
-def catalog(request, category_slug=None):
+class CatalogView(ListView):
+    model = Products
+    # queryset = Products.objects.all().order_by("-id")
+    template_name = "goods/catalog.html"
+    context_object_name = "goods"
+    paginate_by = 3
+    allow_empty = False
 
-    page = request.GET.get('page', 1)
-    on_sale = request.GET.get('on_sale', None)
-    order_by = request.GET.get('order_by', None)
-    query = request.GET.get('q', None)
+    def get_queryset(self):
+        category_slug = self.kwargs.get("category_slug")
+        on_sale = self.request.GET.get("on_sale")
+        order_by = self.request.GET.get("order_by")
+        query = self.request.GET.get("q")
 
-    if category_slug == 'all':
-        goods = Products.objects.all()
-    elif query:
-        goods = q_search(query)
-    else:
-        goods = Products.objects.filter(category__slug=category_slug)
-        if not goods.exists():
-            raise Http404()
+        # Пагинатор
+        if category_slug == 'all':
+            goods = super().get_queryset()
+        elif query:
+            goods = q_search(query)
+        else:
+            goods = super().get_queryset().filter(category__slug=category_slug)
+            if not goods.exists():
+                raise Http404()
 
-    if on_sale:
-        goods = goods.filter(discount__gt=0)
+        if on_sale:
+            goods = goods.filter(discount__gt=0)
 
-    if order_by and order_by != "default":
-        goods = goods.order_by(order_by)
+        if order_by and order_by != "default":
+            goods = goods.order_by(order_by)
 
-    # Вывод 3х товаров
-    paginator = Paginator(goods, 3)
-    current_page = paginator.page(int(page))
+        return goods
 
-    context= {
-        "title": "Home - Каталог",
-        "goods": current_page,
-        "slug_url": category_slug,
-    }
-    return render(request, "goods/catalog.html", context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home - Каталог"
+        context["slug_url"] = self.kwargs.get("category_slug")
+        return context
 
 
 # Отображение конктретного товара
@@ -58,15 +59,5 @@ class ProductView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = self.object.name
+        context["title"] = self.object.name
         return context
-
-# def product(request, product_slug):
-
-#     product = Products.objects.get(slug=product_slug)
-
-#     context = {
-#         'product': product,
-#     }
-
-#     return render(request, "goods/product.html", context)
